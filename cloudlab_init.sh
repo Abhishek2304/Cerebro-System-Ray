cddca16434c89164a6e06361527090a5420e8ee5
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-11-07 22:16:28 -0800

    Added a bash script to generate environment on new cluster

---
+++ cloudlab_init.sh
@@ -0,0 +1,15 @@
+sudo apt install python3-venv
+sudo apt install python3-dev
+
+mkdir project
+cd project
+python3-m venv env_cerebro
+source env_cerebro/bin/activate
+
+pip3 install --upgrade pip
+pip3 install tensorflow==2.3.0
+pip3 install cython
+
+git clone https://github.com/Abhishek2304/cerebro-system.git && cd cerebro-system && make
+
+pip3 install -U 'ray[default]'
