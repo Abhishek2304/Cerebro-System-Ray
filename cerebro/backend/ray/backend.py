c8323db23587733fa7f364b2f493b2d4c04ae6ce
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-11-14 22:07:47 -0800

    Debugging

--- cerebro/backend/ray/backend.py
+++ cerebro/backend/ray/backend.py
@@ -103,7 +103,7 @@ class RayBackend(Backend):
         if self.workers_initialized:
             shard_count = self._num_workers()
             # train_dataset = ray.data.read_csv('/proj/orion-PG0/rayCriteoDataset/train_0.tsv', parse_options =
-                                                csv.ParseOptions(delimiter="\t")) 
+                                                #csv.ParseOptions(delimiter="\t")) 
             train_dataset = ray.data.read_parquet(store.get_train_data_path(dataset_idx)) 
             print(train_dataset)
             self.train_shards = train_dataset.split(n=shard_count, equal = True, locality_hints = self.workers)
