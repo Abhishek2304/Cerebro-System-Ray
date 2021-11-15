471c92f7b9c572ee2cf051ff3709789f2d7d4519
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-11-14 21:53:06 -0800

    Debugging

--- examples/criteo/ray/data_prep.py
+++ examples/criteo/ray/data_prep.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import time
+import ray
 
 from cerebro.backend.ray.backend import RayBackend
 from cerebro.storage import LocalStore
@@ -25,7 +26,17 @@ def main():
     #     label = 'c' + str(i)
     #     header_list.append(label)
 
-    # df = pd.read_csv(data_dir, sep = '\t', names = header_list, header = None)
+    df = pd.read_csv(data_dir, sep = '\t', names = header_list, header = None)
+    print("Reading Done")
+    df.to_csv("/proj/orion-PG0/rayCriteoDataset/train_data.csv")
+    print("Writing Done")
+
+    df1 = pd.read_csv("/proj/orion-PG0/rayCriteoDataset/train_data.csv")
+    print(df1.head())
+    print(len(df1))
+
+    df2 = ray.data.read_csv("/proj/orion-PG0/rayCriteoDataset/train_data.csv")
+    print(df2.show(5))
 
     # print("Removing categorical features for now")
     # for i in range(26):
@@ -36,15 +47,15 @@ def main():
     # print("SAMPLING")
     # df = df.sample(frac = 0.1)
 
-    print("STARTING BACKEND NOW")
-    backend = RayBackend(num_workers = 4)
-    store = LocalStore(OUTPUT_PATH, train_path=os.path.join(OUTPUT_PATH, 'train_data.parquet'), val_path=os.path.join(OUTPUT_PATH, 'val_data.parquet'))
+    # print("STARTING BACKEND NOW")
+    # backend = RayBackend(num_workers = 4)
+    # store = LocalStore(OUTPUT_PATH, train_path=os.path.join(OUTPUT_PATH, 'train_data.parquet'), val_path=os.path.join(OUTPUT_PATH, 'val_data.parquet'))
 
-    #train_rows, val_rows, metadata, _ = backend.prepare_data(store, df, 0.2)
-    backend.initialize_data_loaders(store)
-    print("Initialization done")
-    print()
-    backend.teardown_workers()
+    # #train_rows, val_rows, metadata, _ = backend.prepare_data(store, df, 0.2)
+    # backend.initialize_data_loaders(store)
+    # print("Initialization done")
+    # print()
+    # backend.teardown_workers()
 
 if __name__ == "__main__":
     main()
