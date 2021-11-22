b290e3e4a8375f95f070fa6b2e45ab7e432dc124
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-11-21 20:26:20 -0800

    Debugging

--- cerebro/backend/ray/backend.py
+++ cerebro/backend/ray/backend.py
@@ -135,17 +135,14 @@ class RayBackend(Backend):
             
             train_dataset = ray.data.read_parquet(store.get_train_data_path(dataset_idx), parallelism=1000) 
             self.train_shards = train_dataset.split(n=shard_count, equal=True, locality_hints=self.workers)
+            print(self.train_shards)
+            print(self.train_shards[0].show(5))
             
             val_dataset = ray.data.read_parquet(store.get_val_data_path(dataset_idx)) 
             self.val_shards = val_dataset.split(n=shard_count, equal=True, locality_hints=self.workers)
 
-            print()
-            print()
-            print("Out of Val")
             self.data_loaders_initialized = True
-            print("Out of Dataloader")
-            print()
-            print()
+
         else:
             raise Exception('Ray tasks not initialized for Cerebro. Please run RayBackend.initialize_workers() first!')
 
