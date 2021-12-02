3f420149026fb778173657155b0077dbb66014ff
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-01 17:45:09 -0800

    Preparing mnist data for cerebro

--- examples/mnist/model_selection.py
+++ examples/mnist/model_selection.py
@@ -56,67 +56,71 @@ def estimator_gen_fn(params): # lr, lambda_value
 
 def main():
 
-    OUTPUT_PATH = "/proj/orion-PG0/rayMnistDataset/"
-    NUM_PARTITIONS = 4
-
-    print("STARTING BACKEND NOW")
-    backend = RayBackend(num_workers=NUM_PARTITIONS)
-    store = LocalStore(OUTPUT_PATH, train_path=os.path.join(OUTPUT_PATH, 'train_data.parquet'), \
-        val_path=os.path.join(OUTPUT_PATH, 'val_data.parquet'))
-
-    param_grid_criteo = {
-        "lr": hp_choice([1e-3, 1e-4]),
-        "lambda_value": hp_choice([1e-4, 1e-5]),
-        # "batch_size": hp_choice([32, 64, 256, 512]),
-    }
-
-    model_selection = GridSearch(backend, store, estimator_gen_fn, param_grid_criteo, 5, evaluation_metric='acc',
-                        feature_columns=['features'], label_columns=['label'], verbose=1)
-
-    begin_time = time.time()
-    model = model_selection.fit_on_prepared_data()
-    time_taken = time.time() - begin_time
-
-    print(model.get_best_model_history())
-    
-    return time_taken
-
     # OUTPUT_PATH = "/proj/orion-PG0/rayMnistDataset/"
-    # data_dir = "/proj/orion-PG0/rayMnistDataset/mnist_train.csv"
-    # val_dir = "/proj/orion-PG0/rayMnistDataset/mnist_test.csv"
-    
-    # header_list = ['label']
-    # for i in range(784):
-    #     label = 'n' + str(i)
-    #     header_list.append(label)
-    
-    # df1 = pd.read_csv(data_dir, names = header_list, header = None)
-    # df2 = pd.read_csv(val_dir, names = header_list, header = None)
-    # df = pd.concat([df1, df2])
-    # print("Reading Done")
-
-    # print("Preparing dataset")
-    # final_col_list = list()
-    # for i in range(784):
-    #     final_col_list.append('n' + str(i))
-    
-    # df['features'] = df[final_col_list].values.tolist()
-    
-    # for i in range(784):
-    #     label = 'n' + str(i)
-    #     df = df.drop(columns = [label])
-
-    # df_tar = df.pop('label')
-    # df_tar_one_hot = np.zeros((df_tar.size, df_tar.max()+1))
-    # df_tar_one_hot[np.arange(df_tar.size),df_tar] = 1
-    # df['label'] = df_tar_one_hot.tolist()
+    # NUM_PARTITIONS = 4
 
     # print("STARTING BACKEND NOW")
-    # backend = RayBackend(num_workers = 4)
-    # store = LocalStore(OUTPUT_PATH, train_path=os.path.join(OUTPUT_PATH, 'train_data.parquet'), val_path=os.path.join(OUTPUT_PATH, 'val_data.parquet'))
+    # backend = RayBackend(num_workers=NUM_PARTITIONS)
+    # store = LocalStore(OUTPUT_PATH, train_path=os.path.join(OUTPUT_PATH, 'train_data.parquet'), \
+    #     val_path=os.path.join(OUTPUT_PATH, 'val_data.parquet'))
 
-    # train_rows, val_rows, metadata, _ = backend.prepare_data(store, df, 0.2)
-    # backend.teardown_workers()
+    # param_grid_criteo = {
+    #     "lr": hp_choice([1e-3, 1e-4]),
+    #     "lambda_value": hp_choice([1e-4, 1e-5]),
+    #     # "batch_size": hp_choice([32, 64, 256, 512]),
+    # }
+
+    # model_selection = GridSearch(backend, store, estimator_gen_fn, param_grid_criteo, 5, evaluation_metric='acc',
+    #                     feature_columns=['features'], label_columns=['label'], verbose=1)
+
+    # begin_time = time.time()
+    # model = model_selection.fit_on_prepared_data()
+    # time_taken = time.time() - begin_time
+
+    # print(model.get_best_model_history())
+    
+    # return time_taken
+
+    OUTPUT_PATH = "/proj/orion-PG0/rayMnistDataset/"
+    data_dir = "/proj/orion-PG0/rayMnistDataset/mnist_train.csv"
+    val_dir = "/proj/orion-PG0/rayMnistDataset/mnist_test.csv"
+
+    test.loc[test.index.repeat(test.times)]
+    
+    header_list = ['label']
+    for i in range(784):
+        label = 'n' + str(i)
+        header_list.append(label)
+    
+    df1 = pd.read_csv(data_dir, names = header_list, header = None)
+    df2 = pd.read_csv(val_dir, names = header_list, header = None)
+    df = pd.concat([df1, df2])
+    print("Reading Done")
+
+    print("Preparing dataset")
+    final_col_list = list()
+    for i in range(784):
+        final_col_list.append('n' + str(i))
+    
+    df['features'] = df[final_col_list].values.tolist()
+    
+    for i in range(784):
+        label = 'n' + str(i)
+        df = df.drop(columns = [label])
+
+    df_tar = df.pop('label')
+    df_tar_one_hot = np.zeros((df_tar.size, df_tar.max()+1))
+    df_tar_one_hot[np.arange(df_tar.size),df_tar] = 1
+    df['label'] = df_tar_one_hot.tolist()
+
+    df = df.loc(df.index.repeat(10))
+
+    print("STARTING BACKEND NOW")
+    backend = RayBackend(num_workers = 4)
+    store = LocalStore(OUTPUT_PATH, train_path=os.path.join(OUTPUT_PATH, 'train_data.parquet'), val_path=os.path.join(OUTPUT_PATH, 'val_data.parquet'))
+
+    train_rows, val_rows, metadata, _ = backend.prepare_data(store, df, 0.2)
+    backend.teardown_workers()
 
 if __name__ == "__main__":
     
